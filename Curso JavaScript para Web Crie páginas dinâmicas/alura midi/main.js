13a0894e6e198ef2683febf8aed7428284ddd688
Robert <robert.alexandrea7@gmail.com>
2022-03-30 02:06:51 -0300

    Trocado o while por for of

--- Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
+++ Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
@@ -4,12 +4,8 @@ function tocaSom(idTagAudio) {
     document.querySelector(idTagAudio).play();
 }
 
-let contador = 0;
-
-while (contador < listaTeclas.length) {
-    const tecla = listaTeclas[contador];
+for (tecla of listaTeclas) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = () => tocaSom(idAudio);
-    contador++;
 }
