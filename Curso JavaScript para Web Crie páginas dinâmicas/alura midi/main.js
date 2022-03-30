317babbacfbbf9f10553b2a12cd8fa86958b0b81
Robert <robert.alexandrea7@gmail.com>
2022-03-30 02:01:14 -0300

    Utilizando queryselectorall para pegar todas as teclas, utilizando while para atribuir onclick em cada tecla, criada função para performar o toque do som

--- Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
+++ Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
@@ -1,5 +1,15 @@
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+const listaTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idTagAudio) {
+    document.querySelector(idTagAudio).play();
+}
+
+let contador = 0;
+
+while (contador < listaTeclas.length) {
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+    tecla.onclick = () => tocaSom(idAudio);
+    contador++;
 }
