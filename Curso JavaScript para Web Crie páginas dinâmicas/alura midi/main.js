0c4d0ff73f45e7cb0c4618e8d150efdbdaeab77a
Robert <robert.alexandrea7@gmail.com>
2022-04-01 02:00:52 -0300

    Navegação completa, acionando as fucionalidades via teclado

--- Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
+++ Curso JavaScript para Web Crie páginas dinâmicas/alura midi/main.js
@@ -4,8 +4,17 @@ function tocaSom(idTagAudio) {
     document.querySelector(idTagAudio).play();
 }
 
-for (tecla of listaTeclas) {
+for (let i = 0; i < listaTeclas.length; i++) {
+    const tecla = listaTeclas[i];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
     tecla.onclick = () => tocaSom(idAudio);
+
+    tecla.onkeydown = (key) => {
+        if (key.code === 'Space' || key.code === 'Enter') tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = (key) => {
+        if (key.code === 'Space' || key.code === 'Enter') tecla.classList.remove('ativa');
+    }
 }
