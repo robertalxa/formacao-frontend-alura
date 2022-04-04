3a313e98da2f17257dd7d59c674aae90eb352d9c
Robert <robert.alexandrea7@gmail.com>
2022-04-04 20:01:03 -0300

    Base de Exercícios de Array

---
+++ Ex Trabalhando com Arrays/map - filter - reduce/index.js
@@ -0,0 +1,16 @@
+const empresas = [
+    { nome: 'Samsung', valorDeMercado: 50, CEO: 'Kim Hyun Suk', anoDeCriacao: 1938 },
+    { nome: 'Microsoft', valorDeMercado: 415, CEO: 'Satya Nadella', anoDeCriacao: 1975 },
+    { nome: 'Intel', valorDeMercado: 117, CEO: 'Brian Krzanich', anoDeCriacao: 1968 },
+    { nome: 'Facebook', valorDeMercado: 383, CEO: 'Mark Zuckerberg', anoDeCriacao: 2004 },
+    { nome: 'Spotify', valorDeMercado: 30, CEO: 'Daniel Ek', anoDeCriacao: 2006 },
+    { nome: 'Apple', valorDeMercado: 845, CEO: 'Tim Cook', anoDeCriacao: 1976 }
+];
+
+/*
+Exercícios propostos
+
+- Empresas criadas depois dos anos 2000.
+- O nome de cada empresa e de seu CEO.
+- O valor de todas as empresas somadas.
+*/
