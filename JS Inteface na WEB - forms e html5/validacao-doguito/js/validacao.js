61f6859e8f0b553398b8e304f4e7168a3e80cd33
Robert <robert.alexandrea7@gmail.com>
2022-04-11 22:02:21 -0300

    Adicionadas validações na data via JS; Criada fuçao geérica que valida todos os inputs

---
+++ JS Inteface na WEB - forms e html5/validacao-doguito/js/validacao.js
@@ -0,0 +1,27 @@
+export function valida(input){
+    const tipoDeInput = input.dataset.tipo;
+
+    if(validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+    }
+}
+
+const validadores = {
+    dataNascimento: input => validaDataNascimento(input)
+}
+
+function validaDataNascimento(input){
+    const dataRecebida = new Date(input.value);
+    let mensagem = '';
+
+    if(!maior18(dataRecebida)) mensagem = 'Você deve ser maior de 18 anos para se cadastrar';
+
+    input.setCustomValidity(mensagem);
+}
+
+function maior18(data){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear()+18, data.getUTCMonth(), data.getUTCDate());
+
+    return dataMais18 <= dataAtual;
+}
