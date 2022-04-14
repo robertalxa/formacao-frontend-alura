67a9957033e41f9d44f36dacb53db2c513d207d1
Robert <robert.alexandrea7@gmail.com>
2022-04-14 00:13:24 -0300

    Melhoradas as validações dos campos, exibindo mensagens de erro personalizadas de acordo com o campo e o erro cometido em tal campo

--- JS Inteface na WEB - forms e html5/validacao-doguito/js/validacao.js
+++ JS Inteface na WEB - forms e html5/validacao-doguito/js/validacao.js
@@ -4,10 +4,55 @@ export function valida(input){
     if(validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
     }
+
+    if(input.validity.valid){
+        input.parentElement.classList.remove('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = '';
+    } else{
+        input.parentElement.classList.add('input-container--invalido');
+        input.parentElement.querySelector('.input-mensagem-erro').innerHTML = mostraMensagemErro(tipoDeInput, input);
+    }
+}
+
+const tiposDeErro = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError'
+]
+
+const msgsErro = {
+    nome:{
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email:{
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio.',
+        patternMismatch: 'A senha deve conter entre 6 e 12 caracteres, uma letra minuscula, uma maiúscula e um número'
+    },
+    dataNascimento: {
+        valueMissing: 'O campo de data de nascimento não pode estar vazio.',
+        customError: 'Você deve ser maior de 18 anos para se cadastrar'
+    }
 }
 
 const validadores = {
     dataNascimento: input => validaDataNascimento(input)
+}
+
+function mostraMensagemErro(tipoDeInput, input){
+    let mensagem = '';
+
+    tiposDeErro.forEach(erro=>{
+        if(input.validity[erro]){
+            mensagem = msgsErro[tipoDeInput][erro];
+        }
+    })
+
+    return mensagem;
 }
 
 function validaDataNascimento(input){
